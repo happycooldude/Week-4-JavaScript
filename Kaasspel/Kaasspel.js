dcc4529a5fab8c9238b57fe4a9db800cb3edfc21
happycooldude <pieterhuisman2000@gmail.com>
2019-09-23 14:10:57 +0200

    kaasspel javascript gefixt en afgemaakt

--- Kaasspel/Kaasspel.js
+++ Kaasspel/Kaasspel.js
@@ -1,10 +1,10 @@
 var antwoord;
 
-confirm("Is de kaas geel?");
+antwoord = (confirm("Is de kaas geel?"));
 if (antwoord == true){
-  (confirm("zitten er gaten in?"));
+  antwoord = (confirm("zitten er gaten in?"));
   if (antwoord == true){
-    (confirm("Is de kaas belachelijk duur?"));
+    antwoord = (confirm("Is de kaas belachelijk duur?"));
     if (antwoord == true){
       alert("U heeft gekozen voor Emmenthaler");
     } else{
@@ -12,7 +12,7 @@ if (antwoord == true){
     }
 
   } else{
-    (confirm("Is de kaas hard als steen?"));
+    antwoord = (confirm("Is de kaas hard als steen?"));
     if (antwoord == true){
       alert("U heeft gekozen voor Pamigiano Reggiano");
     } else{
@@ -21,20 +21,20 @@ if (antwoord == true){
   }
 
 } else{
-  (confirm("Heeft de kaas blauwe schimmels?"));
+  antwoord = (confirm("Heeft de kaas blauwe schimmels?"));
   if (antwoord == true){
-    (confirm("Heeft de kaas een korst?"));
-    if (antwoord == true){
-      alert("U heeft gekozen voor Camenbert");
-    } else{
-      alert("U heeft gekozen voorMozzarella");
-    }
-  } else{
-    (confirm("heeft de kaas een korst?"));
+    antwoord = (confirm("Heeft de kaas een korst?"));
     if (antwoord == true){
       alert("U heeft gekozen voor Bleu de Rochbaron");
     } else{
       alert("U heeft gekozen voor Foume d'Ambert");
+    }
+    	} else{
+    antwoord = (confirm("heeft de kaas een korst?"));
+      if (antwoord == true){
+      alert("U heeft gekozen voor Camenbert");
+    }   else{
+      alert("U heeft gekozen voor Mozzarella");
     }
   }
 }
